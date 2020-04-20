a6f7db725f33db737c87359c3bc8653dbc4c7e24
Alex <alex.andrul10@gmail.com>
2020-04-20 17:10:05 +0100

    tv-show-title-and-SE

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
+let divRoot = document.getElementById("root");
+let divContainer = document.createElement("div");
+divRoot.appendChild(divContainer);
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((el) => {
+    console.log(el);
+    let divEpisode = document.createElement("div");
+    divEpisode.className = "episode";
+    divContainer.appendChild(divEpisode);
+    let title = document.createElement("h2");
+    divEpisode.appendChild(title);
+    let seasonNumberAndEpisode = document.createElement("p");
+    divEpisode.appendChild(seasonNumberAndEpisode);
+    title.innerHTML = el.name;
+    seasonNumberAndEpisode.innerHTML = `S${el.season
+      .toString()
+      .padStart(2, "0")}E${el.number}`;
+  });
 }
 
 window.onload = setup;
