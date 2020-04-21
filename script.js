a8721145fe3baef98554e0919cac3e4703189ae6
Alex <alex.andrul10@gmail.com>
2020-04-21 20:12:57 +0100

    displaying number of episodes

--- script.js
+++ script.js
@@ -15,11 +15,20 @@ let body = document.querySelector("body");
 body.appendChild(linkTvMaze);
 
 let divRoot = document.getElementById("root");
+
+//search
 let divSearch = document.createElement("div");
 divRoot.appendChild(divSearch).className = "search";
 let h2Search = document.createElement("h4");
 h2Search.className = "title-search";
 divSearch.appendChild(h2Search).innerHTML = "Search";
+let input = document.createElement("input");
+divSearch.appendChild(input);
+let numOfEpisodes = document.createElement("p");
+numOfEpisodes.className = "num-episodes";
+divSearch.appendChild(numOfEpisodes);
+input.setAttribute("id", "input-search");
+input.setAttribute("type", "text");
 
 //Container for episodes
 let divContainer = document.createElement("div");
@@ -46,26 +55,23 @@ function makePageForEpisodes(episodeList) {
     divEpisode.appendChild(summary).innerHTML = el.summary;
   });
 }
-
-//search
-let input = document.createElement("input");
-divSearch.appendChild(input);
-input.setAttribute("id", "input-search");
-input.setAttribute("type", "text");
+let searchBar = document.getElementById("input-search");
+searchBar.addEventListener("keyup", searchInput);
 
 function searchInput() {
-  let searchBar = document.getElementById("input-search");
-  searchBar.addEventListener("keyup", (e) => {
-    let inputValue = e.target.value;
-    Array.from(document.getElementsByClassName("episode")).forEach((div) => {
-      let txt = div.innerText.toLowerCase();
-
-      if (txt.indexOf(inputValue) != -1) {
-        div.style.display = "block";
-      } else {
-        div.style.display = "none";
-      }
-    });
+  let inputValue = searchBar.value.toLowerCase();
+  let count = 0;
+  Array.from(document.getElementsByClassName("episode")).forEach((div) => {
+    let txt = div.innerText.toLowerCase();
+    if (txt.indexOf(inputValue) != -1) {
+      div.style.display = "block";
+      count += 1;
+    } else {
+      div.style.display = "none";
+    }
   });
+  let allDivEpisodes = document.getElementsByClassName("episode").length;
+  numOfEpisodes.innerText = `Displaying ${count}/ ${allDivEpisodes} episodes`;
 }
+
 window.onload = setup;
