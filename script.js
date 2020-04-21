8edcd4afbcd9d63a03c00e5fc15495b404093695
Alex <alex.andrul10@gmail.com>
2020-04-21 18:10:17 +0100

    done-search

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  searchInput();
 }
 
 //link to TvMaze
@@ -13,8 +14,14 @@ linkTvMaze.target = "_blank";
 let body = document.querySelector("body");
 body.appendChild(linkTvMaze);
 
-//Containwe for episodes
 let divRoot = document.getElementById("root");
+let divSearch = document.createElement("div");
+divRoot.appendChild(divSearch).className = "search";
+let h2Search = document.createElement("h4");
+h2Search.className = "title-search";
+divSearch.appendChild(h2Search).innerHTML = "Search";
+
+//Container for episodes
 let divContainer = document.createElement("div");
 divContainer.className = "container";
 divRoot.appendChild(divContainer);
@@ -32,7 +39,7 @@ function makePageForEpisodes(episodeList) {
       .toString()
       .padStart(2, "0")}E${el.number.toString().padStart(2, "0")}`;
     let title = document.createElement("h2");
-    divEpisode.appendChild(title).innerHTML = `${el.name} `;
+    divEpisode.appendChild(title).innerHTML = el.name;
     let epImg = document.createElement("img");
     divEpisode.appendChild(epImg).src = el.image.medium;
     let summary = document.createElement("p");
@@ -40,4 +47,25 @@ function makePageForEpisodes(episodeList) {
   });
 }
 
+//search
+let input = document.createElement("input");
+divSearch.appendChild(input);
+input.setAttribute("id", "input-search");
+input.setAttribute("type", "text");
+
+function searchInput() {
+  let searchBar = document.getElementById("input-search");
+  searchBar.addEventListener("keyup", (e) => {
+    let inputValue = e.target.value;
+    Array.from(document.getElementsByClassName("episode")).forEach((div) => {
+      let txt = div.innerText.toLowerCase();
+
+      if (txt.indexOf(inputValue) != -1) {
+        div.style.display = "block";
+      } else {
+        div.style.display = "none";
+      }
+    });
+  });
+}
 window.onload = setup;
